8b893c778d699c629cc19dfb9cd57940d4998bbc
irshad1601 <mdirshadalam430@gmail.com>
2025-02-16 21:02:03 +0530

    Update verifyUser.js

--- backend/middlewares/verifyUser.js
+++ backend/middlewares/verifyUser.js
@@ -2,7 +2,7 @@ import { errorHandler } from "../utils/error.js"
 import jwt from "jsonwebtoken"
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token
+  const token = req.cookies.access_token || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return next(errorHandler(401, "Unauthorized"))
