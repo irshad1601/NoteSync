6932115b5525b58e3b8c23ef7a26a68b7bcc8b37
irshad1601 <mdirshadalam430@gmail.com>
2025-02-16 19:43:10 +0530

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
   // get all notes
   const getAllNotes = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/note/all", {
+      const res = await axios.get("https://notesync-xoab.onrender.com/api/note/all", {
         withCredentials: true,
       })
 
@@ -69,7 +69,7 @@ const Home = () => {
 
     try {
       const res = await axios.delete(
-        "http://localhost:5000/api/note/delete/" + noteId,
+        "https://notesync-xoab.onrender.com/api/note/delete/" + noteId,
         { withCredentials: true }
       )
 
@@ -87,7 +87,7 @@ const Home = () => {
 
   const onSearchNote = async (query) => {
     try {
-      const res = await axios.get("http://localhost:5000/api/note/search", {
+      const res = await axios.get("https://notesync-xoab.onrender.com/api/note/search", {
         params: { query },
         withCredentials: true,
       })
@@ -115,7 +115,7 @@ const Home = () => {
 
     try {
       const res = await axios.put(
-        "http://localhost:5000/api/note/update-note-pinned/" + noteId,
+        "https://notesync-xoab.onrender.com/api/note/update-note-pinned/" + noteId,
         { isPinned: !noteData.isPinned },
         { withCredentials: true }
       )
